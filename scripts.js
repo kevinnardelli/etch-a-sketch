c1ebd9a29e4fcc700be9d0cd4a0e7eb2df7cc554
Kevin <138638240+kevinnardelli@users.noreply.github.com>
2023-08-23 12:22:57 -0300

    Add customizable grid size

--- scripts.js
+++ scripts.js
@@ -4,16 +4,33 @@ container.classList.add('container')
 const body = document.querySelector('body')
 body.appendChild(container)
 
-const grids = []
+let grids = []
+numberOfGridsButton = document.querySelector('#number-of-grids')
+numberOfGridsButton.addEventListener('click', () => {
+  if (grids.length > 0) {
+    grids.forEach((grid) => {
+      container.removeChild(grid)
+    })
+  }
 
-for (let i = 0; i < 256; i++) {
-  grids[i] = document.createElement('div')
-  grids[i].classList.add('grid')
-  container.appendChild(grids[i])
-}
+  grids = []
+  let numberOfGrids
+  
+  do {
+    numberOfGrids = window.prompt('Insert number of square per side (max value: 100)')  
+  } while (numberOfGrids > 100);
 
-grids.forEach( (grid) => {
+  for (let i = 0; i < numberOfGrids*numberOfGrids ; i++) {
+    grids[i] = document.createElement('div')
+    grids[i].classList.add('grid')
+    grids[i].setAttribute('style', `width: ${(1/numberOfGrids)*100}%; 
+    height: ${(1/numberOfGrids)*100}%`)
+    container.appendChild(grids[i])
+  }
+
+  grids.forEach( (grid) => {
   grid.addEventListener('mouseover', () => {
     grid.classList.add('painted-grid')
+  })
   })
 })
